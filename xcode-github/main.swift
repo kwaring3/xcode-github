10abf762e2a251d7fdac89e048aeb4030ecd44a0
kwaring3 <kevinwaring@pursuit.org>
2018-10-05 14:34:45 -0400

    Initial Commit

---
+++ xcode-github/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  xcode-github
+//
+//  Created by Kevin Waring on 10/5/18.
+//  Copyright © 2018 Kevin Waring. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
